0a5d81fccc646b529c22887fff694e14d30ef52e
Mohamed Taha <112260594+MahamedTaha@users.noreply.github.com>
2022-08-27 15:51:32 +0200

    Create ExamScores.java

---
+++ ExamScores.java
@@ -0,0 +1,33 @@
+Based on students’ exam scores, give each of them a rate of appreciation and print the
+result. Ranges for rates of appreciation are listed below
+• Excellent: greater than or equal 85 and less than 100
+• Very good: greater than or equal 75 and less than 85
+• Good: greater than or equal 65 and less than 75
+• Pass: greater than or equal 50 and less than 65
+• Fail: less than 50
+
+
+import java.util.Scanner;
+
+public class Task17 {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        double grade; // variable to store the grade
+        
+       System.out.print("Enter grade : ");
+       grade = in.nextDouble(); // take the grade from the scanner
+       
+       // your conditions
+       
+        
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+}

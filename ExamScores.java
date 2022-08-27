233c77f61a2f8229335c686c4f270a0335b05fc0
Mohamed Taha <112260594+MahamedTaha@users.noreply.github.com>
2022-08-27 16:23:19 +0200

    Update ExamScores.java

--- ExamScores.java
+++ ExamScores.java
@@ -7,3 +7,29 @@ result. Ranges for rates of appreciation are listed below
 • Fail: less than 50
 
     
+import java.util.Scanner;
+
+public class ExamScores {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double grade; // variable to store the grade
+
+        System.out.print("Enter grade : ");
+        grade = in.nextDouble(); // take the grade from the scanner
+
+        if (grade >= 85 && grade < 100) {
+            System.out.print(" Excellent");
+        } else if (grade >= 75 && grade < 85) {
+            System.out.print("very good");
+        } else if (grade >= 65 && grade < 75) {
+            System.out.print("good");
+        } else if (grade >= 50 && grade < 65) {
+            System.out.print("pass");
+        } else {
+            System.out.println("fail");
+        }
+
+    }
+
+}
